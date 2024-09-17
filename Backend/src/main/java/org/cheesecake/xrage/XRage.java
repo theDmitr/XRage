1bbdc61c96ee209e05181bc691030b48409caf77
theDmitr <dmitryfirson@gmail.com>
2024-09-17 11:23:27 +0300

    init spring boot app

---
+++ Backend/src/main/java/org/cheesecake/xrage/XRage.java
@@ -0,0 +1,12 @@
+package org.cheesecake.xrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XRage {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XRage.class, args);
+    }
+}
